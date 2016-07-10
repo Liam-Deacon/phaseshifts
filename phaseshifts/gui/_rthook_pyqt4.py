6c2a3d2ee9a1e8b55d4756356e335949f47aab24
Liam Deacon <liam.m.deacon@gmail.com>
2016-07-10 18:26:09 +0100

    Added runtime hook for PyQt4 to be used with PyInstaller

---
+++ phaseshifts/gui/_rthook_pyqt4.py
@@ -0,0 +1,10 @@
+# PyQt4 runtime hook included for PyInstaller 
+import sip
+
+sip.setapi(u'QDate', 2)
+sip.setapi(u'QDateTime', 2)
+sip.setapi(u'QString', 2)
+sip.setapi(u'QTextStream', 2)
+sip.setapi(u'QTime', 2)
+sip.setapi(u'QUrl', 2)
+sip.setapi(u'QVariant', 2)
