64a35b5c7a6cd8294a8487f335177ea6dfb6354f
Liam Deacon <liam.deacon@lightbytestechnology.co.uk>
2024-01-11 15:44:30 +0000

    chore: Run black on setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# encoding: utf-8
 import os
 import sys
 
@@ -15,77 +14,89 @@ try:
     from numpy.distutils.core import Extension, setup
 except ModuleNotFoundError as err:
     if tuple(sys.version_info[:2]) >= (3, 11):
-        raise NotImplementedError("numpy.distutils has been removed for python {}".format(".".join(sys.version_info[:2])))
+        raise NotImplementedError(
+            "numpy.distutils has been removed for python {}".format(
+                ".".join(sys.version_info[:2])
+            )
+        )
     raise
 
 import sys, os
+
 try:
     import py2exe
 except ImportError:
     py2exe = None
 
 if len(sys.argv) == 1:
-    sys.argv.append('install')
+    sys.argv.append("install")
 
 # build f2py extensions
-f2py_exts = [Extension(name='phaseshifts.lib.libphsh',
-                      extra_compile_args=['-fopenmp'],
-                      extra_link_args=['-lgomp'],
-                      sources=[os.path.join('phaseshifts','lib','libphsh.f')])
-            ]
-    
+f2py_exts = [
+    Extension(
+        name="phaseshifts.lib.libphsh",
+        extra_compile_args=["-fopenmp"],
+        extra_link_args=["-lgomp"],
+        sources=[os.path.join("phaseshifts", "lib", "libphsh.f")],
+    )
+]
+
 dist = setup(
-        name = 'phaseshifts',
-        packages = find_packages(),
-        version='0.1.6-dev',
-        author='Liam Deacon',
-        author_email='liam.m.deacon@gmail.com',
-        license='MIT License',
-        url='https://pypi.python.org/pypi/phaseshifts',
-        description='Python-based version of the Barbieri/Van Hove phase '
-        'shift calculation package for LEED/XPD modelling',
-        long_description=None if not os.path.exists('README.rst') else open('README.rst').read(),
-        classifiers=[
-            'Development Status :: 4 - Beta',
-            'Environment :: Console',
-			'Environment :: X11 Applications :: Qt',  # The end goal is to have Qt or other GUI frontend
-            'Intended Audience :: Science/Research',
-            'License :: OSI Approved :: MIT License',
-            'Operating System :: OS Independent',
-            'Programming Language :: Python',
-            'Topic :: Scientific/Engineering :: Chemistry',
-            'Topic :: Scientific/Engineering :: Physics',
-            ],
-        keywords='phaseshifts atomic scattering muffin-tin diffraction',
-        include_package_data = True,
-        package_data = {
-			# If any package contains *.txt or *.rst files, include them:
-			'' : ['*.txt', '*.rst', '*.pyw', 'ChangeLog'],
-			'lib' : ['lib/*.f', 'lib/*.c', 'lib/*.h'],
-            'gui' : ['gui/*.ui', 'gui/*.bat'],
-            'gui/res' : ['gui/res/*.*']
-			},
-        scripts = [ "phaseshifts/phsh.py" ],
-        install_requires = ['scipy >= 0.7', 'numpy >= 1.3', 'periodictable'],
-		ext_modules = f2py_exts,
-        console=[os.path.join("phaseshifts", "phsh.py")],
-        **(
-            {
-                "options": {
-                    'py2exe': { 
-                        'skip_archive': 1,
-                        'compressed': 0,  
-                        'bundle_files': 2, 
-                        'dist_dir': os.path.join("dist", "py2exe"),
-                        'excludes':['tcl', 'bz2'],
-                        'dll_excludes':['w9xpopen.exe', 'tk85.dll', 'tcl85.dll']
-                    }
+    name="phaseshifts",
+    packages=find_packages(),
+    version="0.1.6-dev",
+    author="Liam Deacon",
+    author_email="liam.m.deacon@gmail.com",
+    license="MIT License",
+    url="https://pypi.python.org/pypi/phaseshifts",
+    description="Python-based version of the Barbieri/Van Hove phase "
+    "shift calculation package for LEED/XPD modelling",
+    long_description=None
+    if not os.path.exists("README.rst")
+    else open("README.rst").read(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Environment :: X11 Applications :: Qt",  # The end goal is to have Qt or other GUI frontend
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering :: Chemistry",
+        "Topic :: Scientific/Engineering :: Physics",
+    ],
+    keywords="phaseshifts atomic scattering muffin-tin diffraction",
+    include_package_data=True,
+    package_data={
+        # If any package contains *.txt or *.rst files, include them:
+        "": ["*.txt", "*.rst", "*.pyw", "ChangeLog"],
+        "lib": ["lib/*.f", "lib/*.c", "lib/*.h"],
+        "gui": ["gui/*.ui", "gui/*.bat"],
+        "gui/res": ["gui/res/*.*"],
+    },
+    scripts=["phaseshifts/phsh.py"],
+    install_requires=["scipy >= 0.7", "numpy >= 1.3", "periodictable"],
+    ext_modules=f2py_exts,
+    console=[os.path.join("phaseshifts", "phsh.py")],
+    **(
+        {
+            "options": {
+                "py2exe": {
+                    "skip_archive": 1,
+                    "compressed": 0,
+                    "bundle_files": 2,
+                    "dist_dir": os.path.join("dist", "py2exe"),
+                    "excludes": ["tcl", "bz2"],
+                    "dll_excludes": ["w9xpopen.exe", "tk85.dll", "tcl85.dll"],
                 }
-             } if py2exe else {}
-        )   
+            }
+        }
+        if py2exe
+        else {}
+    ),
 )
 
 if len(sys.argv) < 2:
     # add build_ext and --inplace flags when performing: `python setup.py`
-    sys.argv.append('build_ext')
-    sys.argv.append('--inplace')
+    sys.argv.append("build_ext")
+    sys.argv.append("--inplace")
