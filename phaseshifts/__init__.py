7fd30ca817ee9ac88aac06816834b88afaabb8b0
Liam Deacon <liam.m.deacon@gmail.com>
2024-01-12 14:17:19 +0000

    Add first attempt to use skbuild backend for f2py

--- phaseshifts/__init__.py
+++ phaseshifts/__init__.py
@@ -1,11 +1,4 @@
-from pkg_resources import get_distribution, DistributionNotFound
+__project__ = "phaseshifts"
+__version__ = "0.1.7"
 
-__project__ = 'phaseshifts'
-__version__ = None  # required for initial installation
-
-try:
-    __version__ = get_distribution(__project__).version
-except DistributionNotFound:
-    VERSION = __project__ + '-' + '(local)'
-else:
-    VERSION = __project__ + '-' + __version__
+VERSION = __project__ + "-" + __version__
