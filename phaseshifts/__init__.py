4c5c1f2915894dca80a59c2b946938c0b998e1da
Liam Deacon <liam.m.deacon@gmail.com>
2024-01-14 19:21:14 +0000

    refactor: Change contact info, make slighly more maintainable

--- phaseshifts/__init__.py
+++ phaseshifts/__init__.py
@@ -1,4 +1,6 @@
 __project__ = "phaseshifts"
 __version__ = "0.1.7"
+__homepage__ = "https://github.com/Liam-Deacon/phaseshifts"
+__contact__ = __homepage__ + "/issues"
 
 VERSION = __project__ + "-" + __version__
