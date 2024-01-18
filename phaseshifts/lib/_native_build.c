7efbddf57450187a547cb93a5c735e2a2deeefac
Liam Deacon <liam.m.deacon@gmail.com>
2024-01-18 09:05:28 +0000

    rework _native_build.c

--- phaseshifts/lib/_native_build.c
+++ phaseshifts/lib/_native_build.c
@@ -6,6 +6,11 @@ void dummy() {
     printf("This is a dummy function to force a platform-specific wheel.\n");
 }
 
+static PyMethodDef module_methods[] = {
+    {"dummy", dummy, METH_VARARGS, ""},
+    {NULL, NULL, 0, NULL}
+};
+
 /* NOTE: inspired by https://gist.github.com/douglas-larocca/099bf7460d853abb7c17 */
 PyMODINIT_FUNC PyInit__native_build(void)
 {
@@ -13,8 +18,8 @@ PyMODINIT_FUNC PyInit__native_build(void)
     PyObject *module;
     static struct PyModuleDef moduledef = {
         PyModuleDef_HEAD_INIT,
-        "_chi2",
-        module_docstring,
+        "_native_build",
+        "This module is a workaround for enforcing platform-specific wheels in Python 3.12. It can be ignored",
         -1,
         module_methods,
         NULL,
