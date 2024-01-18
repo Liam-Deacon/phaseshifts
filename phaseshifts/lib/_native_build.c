876319ab1ac248c417b5d7965cdc42c4d6c4cd45
Liam Deacon <liam.m.deacon@gmail.com>
2024-01-18 08:48:24 +0000

    fix: Make _native_build.c have module definition for Windows py3.12

--- phaseshifts/lib/_native_build.c
+++ phaseshifts/lib/_native_build.c
@@ -1,4 +1,29 @@
 /* HACK: This is a workaround for enforcing platform-specific wheels in Python 3.12 */
-int dummy() {
-    return 0;
+#include <stdio.h>
+#include <Python.h>
+
+void dummy() {
+    printf("This is a dummy function to force a platform-specific wheel.\n");
+}
+
+/* NOTE: inspired by https://gist.github.com/douglas-larocca/099bf7460d853abb7c17 */
+PyMODINIT_FUNC PyInit__native_build(void)
+{
+
+    PyObject *module;
+    static struct PyModuleDef moduledef = {
+        PyModuleDef_HEAD_INIT,
+        "_chi2",
+        module_docstring,
+        -1,
+        module_methods,
+        NULL,
+        NULL,
+        NULL,
+        NULL
+    };
+    module = PyModule_Create(&moduledef);
+    if (!module) return NULL;
+
+    return module;
 }
