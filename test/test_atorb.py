fc9e9ca89b8adb5a5935d0570f99e5851c9fa434
Liam Deacon <liam.m.deacon@gmail.com>
2025-07-25 18:16:04 +0100

    fix(test): Rename test message function to avoid pytest fixture confusion
    ## Affected Areas
    - 🧪 Testing
    
    ## Key Changes
    - 🔄 Renamed `testmsg` to `print_test_message`
    - ✨ Updated function calls to reflect new name

--- test/test_atorb.py
+++ test/test_atorb.py
@@ -10,7 +10,7 @@ Test Atorb - checks whether the Atorb class functions as expected
 import unittest, sys, os
 
 
-def testmsg(text):
+def print_test_message(text):
     sys.stderr.write(str(text) + "...")
     sys.stderr.flush()
 
@@ -18,7 +18,7 @@ def testmsg(text):
 class Test(unittest.TestCase):
     def test_atorb(self):
         """Test Atorb class instance"""
-        testmsg(self.shortDescription())
+        print_test_message(self.shortDescription())
         from phaseshifts.atorb import Atorb
 
         at = Atorb()
@@ -27,17 +27,15 @@ class Test(unittest.TestCase):
     def test_info(self):
         """Test static method Atorb.get_quantum_info"""
         sys.stderr.write("\n")
-        testmsg(self.shortDescription())
+        print_test_message(self.shortDescription())
         from phaseshifts.atorb import Atorb
 
-        self.assertEqual(
-            Atorb.get_quantum_info("3d6"), (3, 2, [1.5, 2.5], [2.4, 3.6])
-        )
+        self.assertEqual(Atorb.get_quantum_info("3d6"), (3, 2, [1.5, 2.5], [2.4, 3.6]))
 
     def test_core_replace(self):
         """Test static method Atorb.replace_core_config"""
         sys.stderr.write("\n")
-        testmsg(self.shortDescription())
+        print_test_message(self.shortDescription())
         from phaseshifts.atorb import Atorb
 
         self.assertTrue(Atorb.replace_core_config("[Ar]"), "1s2 2s2 2p6 3s2 3p6")
@@ -45,10 +43,11 @@ class Test(unittest.TestCase):
     def test_calc(self):
         """Test static method Atorb.gen_input"""
         sys.stderr.write("\n")
-        testmsg(self.shortDescription())
+        print_test_message(self.shortDescription())
         from phaseshifts.atorb import Atorb
 
         import tempfile
+
         file = Atorb.gen_input("C", rel=True, output=tempfile.NamedTemporaryFile().name)
         self.assertTrue(os.path.isfile(file))
 
